52a5cf27a6d8419b1c306913205c8bc668b855ae
Raquel-Rangel <raquelrangelribeiro@gmail.com>
2025-12-06 19:51:04 -0300

    Adicionado enums Ordem e StatusPedido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,24 @@
 package application;
 
+import entities.enums.Ordem;
+import entities.enums.StatusPedido;
+
+import java.util.Date;
+
 public class Program {
+
+    public static void main(String[] args){
+
+        Ordem ordem = new Ordem(1080, new Date(), StatusPedido.PAGAMENTO_PENDENTE);
+
+        System.out.println(ordem);
+
+        StatusPedido os1 = StatusPedido.ENTREGUE;
+
+        StatusPedido os2 = StatusPedido.valueOf("ENTREGADO");
+
+        System.out.println(os1);
+        System.out.println(os2);
+
+    }
 }
